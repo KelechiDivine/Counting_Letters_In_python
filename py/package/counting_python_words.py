a4f2aee4a8bf977eb56c881b6659b634541db797
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-03-03 18:16:21 +0100

    On_going_python_projects

---
+++ py/package/counting_python_words.py
@@ -0,0 +1,22 @@
+a_greeting = "HELLO WORLD"
+
+counting_the_greeting_letters = a_greeting.count("H")
+print ("The count of 'H' is ", counting_the_greeting_letters)
+
+counting_the_greeting_letters_2 = a_greeting.count("E")
+print ("The count of 'E' is ", counting_the_greeting_letters_2)
+
+counting_the_greeting_letters_3 = a_greeting.count("L")
+print ("The count of 'L' is ", counting_the_greeting_letters_3)
+
+counting_the_greeting_letters_4 = a_greeting.count("O")
+print ("The count of 'O' is ", counting_the_greeting_letters_4)
+
+counting_the_greeting_letters_5 = a_greeting.count("W")
+print ("The count of 'W' is ", counting_the_greeting_letters_5)
+
+counting_the_greeting_letters_6 = a_greeting.count("R")
+print ("The count of 'R' is ", counting_the_greeting_letters_6)
+
+counting_the_greeting_letters_7 = a_greeting.count("D")
+print ("The count of 'D' is ", counting_the_greeting_letters_7)
